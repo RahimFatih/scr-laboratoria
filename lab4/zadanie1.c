1dd343733e07dc8915c30d374a0ce664d378df0e
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2021-02-03 21:52:38 +0100

    rozszerzone

--- lab4/zadanie1.c
+++ lab4/zadanie1.c
@@ -1,10 +1,11 @@
+
 #include<stdio.h>
 int main()
 {
 printf("Witam,pozdrawiam");
- unsigned int index =0;
-  for (index; ; index++){
-	sleep(1);
-  }
+unsigned int index =0;
+for (index; ; index++){
+sleep(1);
+}
 return 0;
 }
